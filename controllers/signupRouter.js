64e415cf0f287f23ae45d2d15941c54dc70e878d
sidharthsanu12 <sidharthcs007@gmail.com>
2024-02-09 11:27:38 +0530

    commited

--- controllers/signupRouter.js
+++ controllers/signupRouter.js
@@ -1,17 +1,32 @@
 const express=require("express")
 const signupModel=require("../models/signup.Model")
+const bcrypt=require("bcryptjs")
 
 const router=express.Router()
+
+hashPasswordgenerator=async(pass)=>{
+    const salt=await bcrypt.genSalt(10)
+    return bcrypt.hash(pass,salt)
+}
 
     router.post("/signup",async(req,res)=>{
         let {data}={"data":req.body}
         let password=data.password
+        hashPasswordgenerator(password).then(
+            (hashedPassword)=>{
+                console.log(hashedPassword)
+                data.password=hashedPassword
+                console.log(data)
+                let signup=new signupModel(data)
+                let result= signup.save()
 
-        let signup=new signupModel(data)
-        let result=await signup.save()
-        res.json({
-            status:"success"})
-    })
+                res.json({
+                    status:"success"})
+                })                
+
+            }
+        )
+
     router.get("/viewall",async(req,res)=>{
         let data= await signupModel.find()
         res.json(data)
